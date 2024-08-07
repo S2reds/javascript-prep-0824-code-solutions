d604b74504378dcf00645f42236d7f166b18e9bc
Kyle Park <kpstarry@gmail.com>
2024-08-07 03:14:39 +0000

    Completed prep-javascript-operators-and-expressions exercise

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,41 @@
+const number1 = 10;
+const number2 = 20;
+
+const product = number1 * number2;
+console.log(product);
+console.log(typeof product);
+
+const charge = 100;
+const payment = 50;
+
+const amountRemaining = charge - payment;
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+const tests = 5;
+const assignments = 2;
+const final = 10;
+
+const grade = (tests + assignments + final) / 3;
+console.log(grade);
+console.log(typeof grade);
+
+const firstName = 'Kyle';
+const lastName = 'Park';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+
+const pH = 7;
+const isAcidic = pH > 6 ? true : false;
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+const numberOfSoldiers = 300;
+const isSparta = 300 == numberOfSoldiers;
+console.log(isSparta);
+console.log(typeof isSparta);
+
+let nickname = fullName + ' is the GOAT!';
+console.log(nickname);
+console.log(typeof nickname);
